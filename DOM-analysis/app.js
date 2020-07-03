032b4a77f908af4fd387b101534923d75cf580e2
Christopher Pupo <christopherpupo@Christophers-iMac.local>
2020-07-03 14:15:13 -0400

    nice little DOM recap

---
+++ DOM-analysis/app.js
@@ -0,0 +1,15 @@
+let val; 
+
+val = document; 
+val = document.all; 
+val = document.all[0]; 
+val = document.all.length; 
+val = document.body; 
+val = document.doctype; 
+val = document.domain; 
+val = document.URL; 
+
+val = document.forms; 
+
+
+console.log(val); 
